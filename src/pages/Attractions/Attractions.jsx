ce593a33d1255da4191a0e61413b38edac5ac1ca
Arkhopravo <arkho.psarkar@gmail.com>
2024-06-05 10:48:02 +0530

    update

--- src/pages/Attractions/Attractions.jsx
+++ src/pages/Attractions/Attractions.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import './Atractions.css'
+
+const data = [
+  {
+    
+    sr: '1',
+
+    train_no:'12345',
+    train_name:'',
+    sector:'',
+    frequencies:'',
+    travel_time:""
+  },
+  {
+    
+    sr: '2',
+
+    train_no:'23456',
+    train_name:'',
+    sector:'',
+    frequencies:'',
+    travel_time:""
+  },
+  {
+    
+    sr: '3',
+
+    train_no:'34567',
+    train_name:'',
+    sector:'',
+    frequencies:'',
+    travel_time:""
+  },
+  {
+    
+
+    sr: '4',
+    train_no:'45678',
+    train_name:'',
+    sector:'',
+    frequencies:'',
+    travel_time:""
+  },
+  {
+    
+    sr: '5',
+    train_no:'56789',
+    train_name:'',
+    sector:'',
+    frequencies:'',
+    travel_time:""
+  },
+ 
+]
 
 const Attractions = () => {
   return (
-    <div>Attractions</div>
+    <div className='train'>
+      <img src="https://promos.makemytrip.com/images/VandeBharat-tb-dt-050523.webp" alt="" />
+      <h1>Book tickets for Vande Bharat Express with us hassle-free & enjoy comfortable train journeys.</h1>
+      
+    </div>
   )
 }
 
